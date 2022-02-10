b94930358c2396611ef331f7319b550e99baed7d
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2022-02-10 12:30:50 -0500

    Reformatted SQL statements

--- node/index.js
+++ node/index.js
@@ -6,7 +6,12 @@ const initTable = async (p) => {
   const client = await p.connect()
   console.log('Initializing table...')
   try {
-    const tableInitialized = await client.query('CREATE TABLE IF NOT EXISTS accounts (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), balance INT8)')
+    const tableInitialized = await client.query(
+      `CREATE TABLE IF NOT EXISTS accounts (
+          id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+          balance INT8
+        )`
+    )
     console.log(tableInitialized)
   } catch (err) {
     console.log(err.stack)
@@ -20,7 +25,10 @@ const insertAccounts = async (p) => {
   console.log('Inserting new rows into the accounts table...')
   const balanceValue = [Math.floor(Math.random() * 1000)]
   try {
-    const accountsInserted = await client.query('INSERT INTO accounts (id, balance) VALUES (DEFAULT, $1)', balanceValue)
+    const accountsInserted = await client.query(
+      'INSERT INTO accounts (id, balance) VALUES (DEFAULT, $1)',
+      balanceValue
+    )
     console.log(accountsInserted)
   } catch (err) {
     console.log(err.stack)
